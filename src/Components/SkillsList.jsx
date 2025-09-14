403049ac14ac22844bee142caa46d77afa83e2a4
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-09-14 13:10:43 +0200

    Mise en place de l'arrêt du défilement lors du survol du curseur de la souris sur l'espace de défilement des compétences.

--- src/Components/SkillsList.jsx
+++ src/Components/SkillsList.jsx
@@ -1,21 +1,39 @@
 import '../Styles/theme.scss'
+import { useState } from 'react';
 
 
 function SkillsList({ data, animDirection }) {
 
     const skills = data.skills;
+    const [isHover, setIsHover] = useState(false);
 
     return (
         <>
         <h3 className='competences-title'>{data.title}</h3>
-        <div className='competences-main'>
-            <div className='competences-list-wrapper' style={{ width: `calc(200px * ${skills.length})` }}>
+        <div 
+        className='competences-main'
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        >
+            <div 
+            className='competences-list-wrapper' 
+            style={{ 
+                width: `calc(200px * ${skills.length})`
+            }}>
                 {skills.map((item, index) => (
-                    <span className={`list-item-competence`} style={{ animationDelay: `calc(25s / ${skills.length} * (${skills.length} - ${index + 1}) * -1)`, animationDirection: `${animDirection}` }} key={item.id}><img className='list-icon' src={item.picture} alt={item.name} />{item.name}</span>
+                    <span 
+                    className={`list-item-competence`} 
+                    style={{ animationDelay: `calc(25s / ${skills.length} * (${skills.length} - ${index + 1}) * -1)`, 
+                    animationDirection: `${animDirection}`,
+                    animationPlayState: isHover ? 'paused' : 'running' }} 
+                    key={item.id}
+                    >
+                        <img className='list-icon' src={item.picture} alt={item.name} />
+                        {item.name}
+                    </span>
                 ))}
             </div>
         </div>
-            
         </>
     )
 }
