ff501a908f637607c01b4a6b2ebe41d73c5c2842
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2026-02-26 17:49:56 +0100

    Mise en commentaire du lien vers le CV PDF.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -62,8 +62,8 @@ function Home() {
                                 <Link className='presentation-link' to="https://www.linkedin.com/in/rafikbensadi" target="_blank"
                                 rel="noopener noreferrer"><img className='list-icon' src={LinkedIn} alt='Icone LinkedIn'/>Consulter mon profil LinkedIn</Link>
                                 <Link className='presentation-link' to="https://github.com/Rafikosaure" target="_blank"><img className='list-icon' src={GithubIcon} alt="Icone Github" />Consulter mon compte Github</Link>
-                                <a className='presentation-link' target='_blank' rel='noopener noreferrer' href={CV_File}><img className='list-icon' src={CV_Icon} alt='Icone CV'/>Consulter mon Curriculum Vitae (PDF)</a>
-                                <Link className='presentation-link' to="https://bold.pro/my/rafikbensadi" target="_blank"><img className='list-icon' src={CV_OnlineIcon} alt="Icone CV en ligne" />Consulter mon CV détaillé en ligne</Link>
+                                {/* <a className='presentation-link' target='_blank' rel='noopener noreferrer' href={CV_File}><img className='list-icon' src={CV_Icon} alt='Icone CV'/>Consulter mon Curriculum Vitae (PDF)</a> */}
+                                <Link className='presentation-link' to="https://bold.pro/my/rafikbensadi" target="_blank"><img className='list-icon' src={CV_OnlineIcon} alt="Icone CV en ligne" />Consulter mon CV en ligne</Link>
                             </div>
                         </div>
                     </div>
